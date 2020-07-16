ec9dc692312a159d035172e85200bd652121d061
xv44586 <xv44586@gmail.com>
2020-07-16 23:33:53 +0800

    test ln

--- toolkit4nlp/layers.py
+++ toolkit4nlp/layers.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from toolkit4nlp.backend import K, keras
 from toolkit4nlp.backend import sequence_masking
 from keras.layers import *
-from keras import initializers, layers
+from keras import initializers, layers, activations
 
 
 class Layer(keras.layers.Layer):
@@ -129,13 +129,138 @@ class MultiHeadAttention(Layer):
 
 
 class LayerNormalization(Layer):
+    """(Conditional) Layer Normalization
+    hidden_*系列参数仅为有条件输入时(conditional=True)使用
+    """
+    def __init__(
+        self,
+        center=True,
+        scale=True,
+        epsilon=None,
+        conditional=False,
+        hidden_units=None,
+        hidden_activation='linear',
+        hidden_initializer='glorot_uniform',
+        **kwargs
+    ):
+        super(LayerNormalization, self).__init__(**kwargs)
+        self.center = center
+        self.scale = scale
+        self.conditional = conditional
+        self.hidden_units = hidden_units
+        self.hidden_activation = activations.get(hidden_activation)
+        self.hidden_initializer = initializers.get(hidden_initializer)
+        self.epsilon = epsilon or 1e-12
+
+    def compute_mask(self, inputs, mask=None):
+        if self.conditional:
+            masks = [K.expand_dims(m, 0) for m in mask if m is not None]
+            if len(masks) == 0:
+                return None
+            else:
+                return K.all(K.concatenate(masks, axis=0), axis=0)
+        else:
+            return mask
+
+    def build(self, input_shape):
+        super(LayerNormalization, self).build(input_shape)
+
+        if self.conditional:
+            shape = (input_shape[0][-1],)
+        else:
+            shape = (input_shape[-1],)
+
+        if self.center:
+            self.beta = self.add_weight(
+                shape=shape, initializer='zeros', name='beta'
+            )
+        if self.scale:
+            self.gamma = self.add_weight(
+                shape=shape, initializer='ones', name='gamma'
+            )
+
+        if self.conditional:
+
+            if self.hidden_units is not None:
+                self.hidden_dense = Dense(
+                    units=self.hidden_units,
+                    activation=self.hidden_activation,
+                    use_bias=False,
+                    kernel_initializer=self.hidden_initializer
+                )
+
+            if self.center:
+                self.beta_dense = Dense(
+                    units=shape[0], use_bias=False, kernel_initializer='zeros'
+                )
+            if self.scale:
+                self.gamma_dense = Dense(
+                    units=shape[0], use_bias=False, kernel_initializer='zeros'
+                )
+
+
+    def call(self, inputs):
+        """如果是条件Layer Norm，则默认以list为输入，第二个是condition
+        """
+        if self.conditional:
+            inputs, cond = inputs
+            if self.hidden_units is not None:
+                cond = self.hidden_dense(cond)
+            for _ in range(K.ndim(inputs) - K.ndim(cond)):
+                cond = K.expand_dims(cond, 1)
+            if self.center:
+                beta = self.beta_dense(cond) + self.beta
+            if self.scale:
+                gamma = self.gamma_dense(cond) + self.gamma
+        else:
+            if self.center:
+                beta = self.beta
+            if self.scale:
+                gamma = self.gamma
+
+        outputs = inputs
+        if self.center:
+            mean = K.mean(outputs, axis=-1, keepdims=True)
+            outputs = outputs - mean
+        if self.scale:
+            variance = K.mean(K.square(outputs), axis=-1, keepdims=True)
+            std = K.sqrt(variance + self.epsilon)
+            outputs = outputs / std
+            outputs = outputs * gamma
+        if self.center:
+            outputs = outputs + beta
+
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        if self.conditional:
+            return input_shape[0]
+        else:
+            return input_shape
+
+    def get_config(self):
+        config = {
+            'center': self.center,
+            'scale': self.scale,
+            'epsilon': self.epsilon,
+            'conditional': self.conditional,
+            'hidden_units': self.hidden_units,
+            'hidden_activation': activations.serialize(self.hidden_activation),
+            'hidden_initializer':
+                initializers.serialize(self.hidden_initializer),
+        }
+        base_config = super(LayerNormalization, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class LayerNormalizationV2(Layer):
     """
     [Layer Normalization](https://arxiv.org/pdf/1607.06450.pdf)
     x = beta + (x - mean / std) * gamma
     """
 
     def __init__(self, center=True, scale=True, epsilon=None, **kwargs):
-        super(LayerNormalization, self).__init__(**kwargs)
+        super(LayerNormalizationV2, self).__init__(**kwargs)
         self.center = center
         self.scale = scale
         self.epsilon = epsilon or 1e-12
@@ -143,9 +268,9 @@ class LayerNormalization(Layer):
     def build(self, input_shape):
         super(LayerNormalization, self).build(input_shape)
         if self.center:
-            self.beta = self.add_weight(shape=input_shape[-1], initializer='zero', name='beta')
+            self.beta = self.add_weight(shape=(input_shape[-1], ), initializer='zeros', name='beta')
         if self.scale:
-            self.gamma = self.add_weight(shape=input_shape[-1], initializer='one', name='gamma')
+            self.gamma = self.add_weight(shape=(input_shape[-1], ), initializer='ones', name='gamma')
 
     def call(self, inputs, **kwargs):
         output = inputs
