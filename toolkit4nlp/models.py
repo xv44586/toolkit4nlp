34a69af078892c5485fb9856c736239c1ae080a8
xv44586 <xv44586@163.com>
2020-08-08 17:50:36 +0800

    rebuild transformer and bert

--- toolkit4nlp/models.py
+++ toolkit4nlp/models.py
@@ -22,26 +22,16 @@ class Transformer(object):
                  num_attention_heads,  # Attention头数
                  intermediate_size,  # FeedForward 层的隐层维度
                  hidden_act=None,  # FeedFoward 层的激活函数
-                 hidden_dropout_prob=None,  # FeedForward 层dropout 比例
-                 attentioin_probs_dropout_prob=None,  # attention 层dropout 比例
+                 dropout_rate=None,  # dropout 比例
                  embedding_size=None,  # embedding 层维度
-                 seq_length=None,  # 是否固定序列长度
+                 attention_key_size=None,  # attentioin 中 Q, K 的head size
+                 sequence_length=None,  # 是否固定序列长度
                  attention_mask=None,  # attention 层mask
                  layers=None,  # 外部传人的layer
                  name=None,  # 模型名
                  prefix=None,  # layer name 的前缀
                  **kwargs):
         """
-
-        :param vocab_size:
-        :param hidden_size:
-        :param num_hidden_layers:
-        :param num_attention_heads:
-        :param intermediate_size:
-        :param intermidial_act_fc:
-        :param hidden_dropout_prob:
-        :param attentioin_probs_dropout_prob:
-        :param initializer_range:
         """
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
@@ -50,13 +40,13 @@ class Transformer(object):
         if hidden_size % num_attention_heads != 0:
             raise ValueError('The hidden size {hidden_size} is not a multiple of the number of attention heads {'
                              'num_attention_heads}')
-        self.num_attention_size = hidden_size // num_attention_heads
+        self.attention_head_size = hidden_size // num_attention_heads
+        self.attention_key_size = attention_key_size or self.attention_head_size
         self.intermediate_size = intermediate_size
         self.hidden_act = hidden_act
-        self.hidden_dropout_prob = hidden_dropout_prob
-        self.attention_probs_dropout_prob = attentioin_probs_dropout_prob or hidden_dropout_prob
+        self.dropout_rate = dropout_rate or 0
         self.embedding_size = embedding_size or hidden_size
-        self.seq_length = seq_length
+        self.sequence_length = sequence_length
         self.attention_mask = attention_mask
         self.layers = layers or {}
         self.name = name
@@ -64,14 +54,16 @@ class Transformer(object):
         self.built = False
 
     def build(self, **kwargs):
+        """构建模型
+        """
         if self.built:
-            return
+            return None
 
         # inputs
         inputs = self.get_inputs()
         self.set_inputs(inputs)
         # call
-        outputs = self.call(self.inputs)
+        outputs = self.call(inputs)
         self.set_outputs(outputs)
         # model
         self.model = Model(self.inputs, self.outputs, name=self.name)
@@ -79,44 +71,43 @@ class Transformer(object):
         self.built = True
 
     def call(self, inputs):
+        """模型的计算图
+        """
         # embedding
         outputs = self.apply_embeddings(inputs)
 
         # main transformer layers
         for _ in range(self.num_hidden_layers):
-            outputs = self.apply_transformer(outputs, _)
+            outputs = self.apply_attention_layers(outputs, _)
 
         # task related layers
         outputs = self.apply_task_related(outputs)
         return outputs
 
-    def apply_embeddings(self, inputs):
-        raise NotImplementedError
+    def prefixed(self, name):
+        """增加前缀
+        """
+        if name is not None:
+            return self.prefix + name
 
-    def apply_transformer(self, inputs, idx):
-        raise NotImplementedError
-
-    def apply_task_related(self, inputs):
-        raise NotImplementedError
-
-    def apply(self, inputs=None, layer=None, layer_name=None, arguments=None, **kwargs):
+    def apply(self, inputs=None, layer=None, name=None, arguments=None, **kwargs):
         """
          记录layer信息方便后续mapping权重服务；重用同名layer;
          layer(name=layer_name, **kwargs)(inputs, **arguments)
         :param inputs: 上一层的输出
         :param layer: 具体layer
-        :param layer_name: 层的名字
+        :param name: 层的名字
         :param arguments: 计算时使用参数
         :param kwargs: 初始化参数
         :return:
         """
-        if layer is Dropout and self.hidden_dropout_prob == 0:
+        if layer is Dropout and self.dropout_rate == 0:
             return inputs
 
         arguments = {} if arguments is None else arguments
 
         # add prefix
-        name = self.prefixed(layer_name)
+        name = self.prefixed(name)
         kwargs['name'] = name
 
         if name not in self.layers:
@@ -129,62 +120,24 @@ class Transformer(object):
 
         return self.layers[name](inputs, **arguments)
 
-    @property
-    def initializer(self):
-        """
-        截断正态分布初始化
-        """
-        return keras.initializers.TruncatedNormal(stddev=0.02)
-
     def get_inputs(self):
+        raise NotImplementedError
+
+    def apply_embeddings(self, inputs):
+        raise NotImplementedError
+
+    def apply_attention_layers(self, inputs, idx):
+        raise NotImplementedError
+
+    def apply_task_related(self, inputs):
         raise NotImplementedError
 
     def compute_attention_mask(self, inputs):
         return self.attention_mask
 
-    def load_variable(self, checkpoint, name):
-        return tf.train.load_variable(checkpoint, name)
-
-    def variable_mapping(self):
-        # keras 层与checkpoint变量直接的映射关系
-        return {}
-
-    def load_weights_from_checkpoint(self, checkpoint, mapping=None):
-        mapping = mapping or self.variable_mapping()
-        mapping = {self.prefixed(k): v for k, v in mapping.items() if k in self.layers}
-
-        weights_values_pairs = []
-        for layer_name, variables in mapping.items():
-            weights = self.layers[layer_name].trainable_weights
-            values = [self.load_variable(checkpoint, v) for v in variables]
-
-            weights_values_pairs.extend(zip(weights, values))
-
-        K.batch_set_value(weights_values_pairs)
-
-    def create_variable(self, name, value):
-        return tf.Variable(name=name, initial_value=value)
-
-    def save_weights_as_checkpoint(self, checkpoint_path, mapping=None):
-        mapping = mapping or self.variable_mapping()
-        mapping = {self.prefixed(k): v for k, v in mapping.items() if k in self.layers}
-
-        with tf.Graph().as_default():
-            for layer_name, variables in mapping.items():
-                layer = self.layers[layer_name]
-                weights = K.batch_get_value(layer.trainable_weights)
-                for variable, weight in zip(variables, weights):
-                    self.create_variable(variable, weight)
-            with tf.Session() as sess:
-                sess.run(tf.global_variable_initialzer())
-                saver = tf.train.Saver()
-                saver.save(sess, checkpoint_path, write_meta_graph=False)
-
-    def prefixed(self, name):
-        if name is not None:
-            return self.prefix + name
-
     def set_inputs(self, inputs):
+        """设置input 和 inputs
+        """
         if inputs is None:
             inputs = []
         elif not isinstance(inputs, list):
@@ -198,6 +151,8 @@ class Transformer(object):
             self.input = inputs[0]
 
     def set_outputs(self, outputs):
+        """设置output 和 outputs
+        """
         if not isinstance(outputs, list):
             outputs = [outputs]
 
@@ -208,10 +163,61 @@ class Transformer(object):
         else:
             self.output = outputs[0]
 
+    @property
+    def initializer(self):
+        """
+        截断正态分布初始化
+        """
+        return keras.initializers.TruncatedNormal(stddev=0.02)
+
+    def load_variable(self, checkpoint, name):
+        return tf.train.load_variable(checkpoint, name)
+
+    def create_variable(self, name, value):
+        return tf.Variable(initial_value=value, name=name)
+
+    def variable_mapping(self):
+        # keras 层与checkpoint变量直接的映射关系
+        return {}
+
+    def load_weights_from_checkpoint(self, checkpoint, mapping=None):
+        """根据 mapping 从 checkpoint 加载权重
+        """
+        mapping = mapping or self.variable_mapping()
+        mapping = {self.prefixed(k): v for k, v in mapping.items()}
+        mapping = {k: v for k, v in mapping.items() if k in self.layers}
+
+        weights_values_pairs = []
+        for layer_name, variables in mapping.items():
+            weights = self.layers[layer_name].trainable_weights
+            values = [self.load_variable(checkpoint, v) for v in variables]
+
+            weights_values_pairs.extend(zip(weights, values))
+
+        K.batch_set_value(weights_values_pairs)
+
+    def save_weights_as_checkpoint(self, checkpoint_path, mapping=None):
+        """根据mapping 将权重保存为 checkpoint格式
+        """
+        mapping = mapping or self.variable_mapping()
+        mapping = {self.prefixed(k): v for k, v in mapping.items()}
+        mapping = {k: v for k, v in mapping.items() if k in self.layers}
+
+        with tf.Graph().as_default():
+            for layer_name, variables in mapping.items():
+                layer = self.layers[layer_name]
+                weights = K.batch_get_value(layer.trainable_weights)
+                for variable, weight in zip(variables, weights):
+                    self.create_variable(variable, weight)
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+                saver = tf.train.Saver()
+                saver.save(sess, checkpoint_path, write_meta_graph=False)
+
 
 class BERT(Transformer):
     """
-
+    Google Bert
     """
 
     def __init__(self,
@@ -233,20 +239,21 @@ class BERT(Transformer):
 
     def get_inputs(self):
         token_in = self.apply(layer=Input,
-                              layer_name='Input-Token',
-                              shape=(self.seq_length,))
+                              name='Input-Token',
+                              shape=(self.sequence_length,))
         segment_in = self.apply(layer=Input,
-                                layer_name='Input-Segment',
-                                shape=(self.seq_length,))
+                                name='Input-Segment',
+                                shape=(self.sequence_length,))
 
         return [token_in, segment_in]
 
     def apply_embeddings(self, inputs):
-        """token_embedding + segment_embedding + position_embedding"""
+        """token_embedding + segment_embedding + position_embedding
+        """
         x, s = inputs[:2]
         token_embedding = self.apply(inputs=x,
                                      layer=Embedding,
-                                     layer_name='Embedding-Token',
+                                     name='Embedding-Token',
                                      input_dim=self.vocab_size,
                                      output_dim=self.embedding_size,
                                      embeddings_initializer=self.initializer,
@@ -254,35 +261,32 @@ class BERT(Transformer):
                                      )
         segment_embedding = self.apply(s,
                                        Embedding,
-                                       layer_name='Embedding-Segment',
+                                       name='Embedding-Segment',
                                        input_dim=2,
                                        output_dim=self.embedding_size,
                                        embeddings_initializer=self.initializer,
                                        )
-        token_with_seg = self.apply([token_embedding, segment_embedding], Add, 'Embedding-Token-Segment')
+        token_with_seg = self.apply([token_embedding, segment_embedding], Add, name='Embedding-Token-Segment')
         x = self.apply(token_with_seg,
-                       PositionEmbedding, 'Embedding-Position',
+                       PositionEmbedding, name='Embedding-Position',
                        input_dim=self.max_position,
                        output_dim=self.embedding_size,
                        embeddings_initializer=self.initializer,
                        merge_mode='add')
 
         x = self.apply(x,
-                       LayerNormalization, 'Embedding-Norm')
+                       LayerNormalization, name='Embedding-Norm')
         x = self.apply(x, Dropout,
-                       'Embedding-Dropout', rate=self.hidden_dropout_prob)
+                       name='Embedding-Dropout', rate=self.dropout_rate)
         if self.hidden_size != self.embedding_size:
-            x = self.apply(x, Dense, 'Embedding-Mapping',
+            x = self.apply(x, Dense, name='Embedding-Mapping',
                            units=self.hidden_size, kernel_initializer=self.initializer)
 
         return x
 
-    def apply_transformer(self, inputs, idx):
+    def apply_attention_layers(self, inputs, idx):
         """
         Att --> Dropout --> Add --> LN --> FFN --> Dropout -->  Add --> LN
-        :param idx:
-        :param inputs:
-        :return:
         """
         attention_name = 'Transformer-%d-MultiHeadSelfAttention' % idx
         feed_forward_name = 'Transformer-%d-FeedForward' % idx
@@ -297,43 +301,43 @@ class BERT(Transformer):
         # self-attention
         x = self.apply(x,
                        MultiHeadAttention,
-                       attention_name,
+                       name=attention_name,
                        head_nums=self.num_attention_heads,
-                       head_size=self.num_attention_size,
+                       head_size=self.attention_head_size,
                        arguments=arguments,
                        kernel_initializer=self.initializer)
         x = self.apply(x,
                        Dropout,
-                       '%s-Dropout' % attention_name,
-                       rate=self.attention_probs_dropout_prob)
+                       name='%s-Dropout' % attention_name,
+                       rate=self.dropout_rate)
 
         x = self.apply([x_pre, x],
                        Add,
-                       '%s-Add' % attention_name
+                       name='%s-Add' % attention_name
                        )
 
         x = self.apply(x,
                        LayerNormalization,
-                       '%s-Norm' % attention_name,
+                       name='%s-Norm' % attention_name,
                        )
 
         # feedforward
         x_pre = x
         x = self.apply(x,
                        FeedForward,
-                       feed_forward_name,
+                       name=feed_forward_name,
                        units=self.intermediate_size,
                        activation=self.hidden_act,
                        kernel_initializer=self.initializer
                        )
         x = self.apply(x,
                        Dropout,
-                       '%s-Dropout' % feed_forward_name,
-                       rate=self.hidden_dropout_prob)
+                       name='%s-Dropout' % feed_forward_name,
+                       rate=self.dropout_rate)
         x = self.apply([x_pre, x],
                        Add,
-                       '%s-Add' % feed_forward_name)
-        x = self.apply(x, LayerNormalization, '%s-Norm' % feed_forward_name)
+                       name='%s-Add' % feed_forward_name)
+        x = self.apply(x, LayerNormalization, name='%s-Norm' % feed_forward_name)
         return x
 
     def apply_task_related(self, inputs):
@@ -385,14 +389,6 @@ class BERT(Transformer):
         """加载单个变量的函数
         """
         variable = super(BERT, self).load_variable(checkpoint, name)
-        # if name in [
-        #     'bert/embeddings/word_embeddings',
-        #     'cls/predictions/output_bias',
-        # ]:
-        #     if self.keep_tokens is None:
-        #         return variable
-        #     else:
-        #         return variable[self.keep_tokens]
         if name == 'cls/seq_relationship/output_weights':
             return variable.T
         else:
