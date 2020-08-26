1adc934278276e10fc3a0321a389e398055d4986
xv44586 <xv44586@gmail.com>
2020-08-26 14:27:30 +0800

    add search layer

--- toolkit4nlp/backend.py
+++ toolkit4nlp/backend.py
@@ -121,6 +121,40 @@ def leaky_relu(x, alpha=0.2):
     return tf.nn.leaky_relu(x, alpha=alpha)
 
 
+def search_layer(inputs, name, exclude_from=None):
+    """根据inputs和name来搜索层
+    说明：inputs为某个层或某个层的输出；name为目标层的名字。
+    实现：根据inputs一直往上递归搜索，直到发现名字为name的层为止；
+         如果找不到，那就返回None。
+    """
+    if exclude_from is None:
+        exclude_from = set()
+
+    if isinstance(inputs, keras.layers.Layer):
+        layer = inputs
+    else:
+        layer = inputs._keras_history[0]
+
+    if layer.name == name:
+        return layer
+    elif layer in exclude_from:
+        return None
+    else:
+        exclude_from.add(layer)
+        if isinstance(layer, keras.models.Model):
+            model = layer
+            for layer in model.layers:
+                if layer.name == name:
+                    return layer
+        inbound_layers = layer._inbound_nodes[0].inbound_layers
+        if not isinstance(inbound_layers, list):
+            inbound_layers = [inbound_layers]
+        if len(inbound_layers) > 0:
+            for layer in inbound_layers:
+                layer = search_layer(layer, name, exclude_from)
+                if layer is not None:
+                    return layer
+
 # 给旧版本keras新增symbolic方法（装饰器），
 # 以便兼容optimizers.py中的代码
 K.symbolic = getattr(K, 'symbolic', None) or symbolic
