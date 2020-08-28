6cfe6750fb24ea6dc1b3a2f0e37c4ee6a856d9c2
xv44586 <xv44586@163.com>
2020-08-28 16:20:47 +0800

    complete question generation

--- examples/qa_question_generation_seq2seq.py
+++ examples/qa_question_generation_seq2seq.py
@@ -11,6 +11,7 @@ import json
 import numpy as np
 from tqdm import tqdm
 from collections import defaultdict
+from rouge import Rouge
 
 from toolkit4nlp.backend import K, keras
 from toolkit4nlp.models import build_transformer_model, Model
@@ -24,7 +25,7 @@ from keras.callbacks import Callback
 # 基本信息
 maxlen = 512
 epochs = 5
-batch_size = 4
+batch_size = 8
 learning_rate = 2e-5
 max_question_len = 32
 
@@ -67,7 +68,7 @@ class data_generator(DataGenerator):
     """
 
     def __iter__(self):
-        batch_token_ids, batch_segment_ids, batch_labels = [], [], []
+        batch_token_ids, batch_segment_ids, batch_original_token_ids = [], [], []
         for is_end, item in self.get_sample():
             context, question, answers = item[1:]
             answer = np.random.choice(answers)
@@ -78,13 +79,22 @@ class data_generator(DataGenerator):
             token_ids = token_ids + question_token_ids
             segment_ids += [1] * len(question_token_ids)
 
+            original_tokens = token_ids
+            # random replace decoder tokens to generate negative sample
+            is_negative = np.random.random() > 0.5
+            if is_negative:
+                token_ids = [token if seg == 0 or np.random.random() > 0.3 else np.random.choice(token_ids) for
+                             token, seg in zip(token_ids, segment_ids)]  # 0.5 概率替换，其中0.3的概率替换
             batch_token_ids.append(token_ids)
             batch_segment_ids.append(segment_ids)
+            batch_original_token_ids.append(original_tokens)
+
             if len(batch_token_ids) == self.batch_size or is_end:
                 batch_token_ids = pad_sequences(batch_token_ids)
                 batch_segment_ids = pad_sequences(batch_segment_ids)
-                yield [batch_token_ids, batch_segment_ids], None
-                batch_token_ids, batch_segment_ids = [], []
+                batch_original_token_ids = pad_sequences(batch_original_token_ids)
+                yield [batch_token_ids, batch_segment_ids, batch_original_token_ids], None
+                batch_token_ids, batch_segment_ids, batch_original_token_ids = [], [], []
 
 
 # loss 层，错位计算预测值并mask掉segment1
@@ -106,17 +116,24 @@ model = build_transformer_model(
     application='unilm',
     keep_tokens=keep_tokens
 )
-
-output = CrossEntropy(2)(model.inputs + model.outputs)
-
-model = Model(model.inputs, output)
-model.compile(optimizer=Adam(1e-5))
 model.summary()
+
+# train model
+o_inputs = Input(shape=(None,))
+train_model = Model(model.inputs + [o_inputs], model.outputs + [o_inputs])
+y_true = train_model.inputs[2][:, 1:]
+y_mask = train_model.inputs[1][:, 1:]
+y_pred = train_model.outputs[0][:, :-1]
+cross_entropy = K.sparse_categorical_crossentropy(y_true, y_pred)
+cross_entropy = K.sum(cross_entropy * y_mask) / K.sum(y_mask)
+train_model.add_loss(cross_entropy)
+train_model.compile(Adam(1e-5))
 
 
 class QuestionGenerator(AutoRegressiveDecoder):
     """seq2seq解码器
     """
+
     @AutoRegressiveDecoder.wraps('probas')
     def predict(self, inputs, output_ids, states):
         token_ids, segment_ids = inputs
@@ -154,7 +171,29 @@ def generate_question(context, answer):
     return questions
 
 
-def evaluate(valid_data):
+def evaluate(data, topk=1):
+    total = 0
+    rouge = Rouge()
+    rouge_1, rouge_2, rouge_l = 0, 0, 0
+    for d in tqdm(data):
+        _, context, question, answers = d
+        pred_question = extract_question(context, answers[0])
+        total += 1
+        question = ' '.join(question)
+        pred_question = ' '.join(pred_question)
+        if pred_question.strip():
+            scores = rouge.get_scores(hyps=pred_question, refs=question)
+            rouge_1 += scores[0]['rouge-1']['f']
+            rouge_2 += scores[0]['rouge-2']['f']
+            rouge_l += scores[0]['rouge-l']['f']
+
+    rouge_1 /= total
+    rouge_2 /= total
+    rouge_l /= total
+    return rouge_1, rouge_2, rouge_l
+
+
+def evaluate_simple(valid_data):
     """rouge """
     T, R, P = 0., 0., 0.
     for d in tqdm(valid_data):
@@ -185,43 +224,51 @@ def just_show():
 class Evaluator(Callback):
     def __init__(self, **kwargs):
         super(Evaluator, self).__init__(**kwargs)
-        self.best_f1 = 0.
+        self.best_rouge_1 = 0.
+        self.best_rouge_2 = 0.
+        self.best_rouge_l = 0.
 
-    def on_batch_end(self, batch, logs=None):
-        f1, _, _ = evaluate(val_data)
-        if f1 > self.best_f1:
-            self.best_f1 = f1
-            self.model.save_weights('best_model.weights')
+    def on_epoch_end(self, batch, logs=None):
+        rouge_1, rouge_2, rouge_l = evaluate(val_data)
+        if rouge_l > self.best_rouge_l:
+            self.best_rouge_l = rouge_l
+            self.best_rouge_2 = rouge_2
+            self.best_rouge_1 = rouge_1
+            self.model.save_weights('qa_generator_negative.weights')
 
-        print('epoch {}: f1: {}, best f1: {}'.format(epochs, f1, self.best_f1))
+        print('epoch {}: rouge_l: {}, best rouge_l: {}, best_rouge_2: {}, best_rouge_l: {}'.format(
+            epochs, rouge_l, self.best_rouge_1, self.best_rouge_2, self.best_rouge_l))
         just_show()
 
 
-if __name__ == '__main__':
-    train_generator = data_generator(train_data, batch_size)
-    evaluator = Evaluator()
-    model.fit_generator(
-        train_generator.generator(),
-        steps_per_epoch=len(train_generator),
-        epochs=epochs,
-        callbacks=[evaluator])
+def intersect(s1, s2):
+    common = len(set(s1) & set(s2))
+    l2 = len(set(s2))
+    if not l2 > 0:
+        return 0.
+    return 1.0 * common / l2
 
+
+def generate_new_data(random=False, file_name='train_augment.json'):
     # generate new questions
-    model.load_weights('best_model.weights')
-    new_train_data = []
     tem = defaultdict(list)
     for d in tqdm(train_data):
         qas = []
         _id, context, question, answers = d
         new_questions = [question]
-        for answer in answers:
-            g_questions = generate_question(context, answer)
-            g_questions = [q for q in set(g_questions) if q not in new_questions]
-            new_questions.extend(g_questions)
+        answer = np.random.choice(answers)
+        # 随机抽样 or beam search
+        if random:
+            g_question = generate_question(context, answer)
+        else:
+            g_question = extract_question(context, answer)
+
+        if g_question not in new_questions:
+            new_questions.append(g_question)
+
         answers = [{'text': a} for a in answers]
-        for q in new_questions:
-            qa = {'id': _id, 'question': q, 'answers': answers}
-            qas.append(qa)
+        qa = {'id': _id, 'question': new_questions, 'answers': answers}
+        qas.append(qa)
 
         tem[context].extend(qas)
 
@@ -230,9 +277,25 @@ if __name__ == '__main__':
         paragraphs.append({'context': c, 'qas': qas})
 
     data = {'data': [{'paragraphs': paragraphs}]}
-
-    with open('/home/mingming.xu/datasets/NLP/qa/dureader_robust-data/new_train.json', 'w') as f:
+    with open(file_name, 'w') as f:
         json.dump(data, f)
+
+
+if __name__ == '__main__':
+    train_generator = data_generator(train_data, batch_size)
+    evaluator = Evaluator()
+
+    train_model.fit_generator(train_generator.generator(),
+                       steps_per_epoch=len(train_generator),
+                       epochs=epochs,
+                       callbacks=[evaluator])
+
+    # generate new questions
+    file_name = '/home/mingming.xu/datasets/NLP/qa/dureader_robust-data/train_random.json'
+    generate_new_data(random=True, file_name=file_name)
 
 else:
     model.load_weights('best_model.weights')
+    # generate new questions
+    file_name = '/home/mingming.xu/datasets/NLP/qa/dureader_robust-data/train_random.json'
+    generate_new_data(True, file_name)
