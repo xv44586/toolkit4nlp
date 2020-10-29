a82308e31d811e6dcf61b9d98cfc30ca13c2aec8
xv44586 <xv44586@gmail.com>
2020-10-29 13:14:46 +0800

    update ccf result

--- examples/ccf_2020_qa_match_point.py
+++ examples/ccf_2020_qa_match_point.py
@@ -5,7 +5,8 @@
 # @File    : ccf_2020_qa_match_point.py
 """
 主要思路：将reply顺序拼接到query后面，利用每个reply的[SEP]token做二分类
-best val f1: 78.7
+best val f1: 0.794
+A 榜: 0.756
 """
 import os
 from tqdm import tqdm
@@ -127,29 +128,53 @@ class ClsMerge(Layer):
         return mask
 
 
+class ConcatSeq2Vec(Layer):
+    def __init__(self, **kwargs):
+        super(ConcatSeq2Vec, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        super(ConcatSeq2Vec, self).build(input_shape)
+
+    def call(self, x):
+        seq, vec = x
+        vec = K.expand_dims(vec, 1)
+        vec = K.tile(vec, [1, K.shape(seq)[1], 1])
+        return K.concatenate([seq, vec], 2)
+
+    def compute_mask(self, inputs, mask):
+        return mask[0]
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0][:-1] + (input_shape[0][-1] + input_shape[1][-1],)
+
+
 bert = build_transformer_model(
     config_path=config_path,
     checkpoint_path=checkpoint_path,
-    model='nezha'
+    model='nezha',
 )
 
 m_inputs = Input(shape=(None,))
 label_inputs = Input(shape=(None,))
 output = bert.output
-output = ClsMerge()(output)
+
 output = Dropout(0.5)(output)
-output = Dense(768, activation='tanh')(output)
+output = Dense(384, activation='tanh')(output)
+att = AttentionPooling1D(name='attention_pooling_1')(output)
+output = ConcatSeq2Vec()([output, att])
+
+output = DGCNN(dilation_rate=1, dropout_rate=0.1)(output)
+output = DGCNN(dilation_rate=2, dropout_rate=0.1)(output)
+output = DGCNN(dilation_rate=5, dropout_rate=0.1)(output)
 
 output = Dense(1, activation='sigmoid')(output)
-
 output = Lambda(lambda x: x[:, :, 0])(output)
-
 x = PointLoss(0)([output, label_inputs, m_inputs])
 
 train_model = Model(bert.inputs + [label_inputs, m_inputs], x)
 
 infer_model = Model(bert.inputs, output)
-train_model.compile(optimizer=Adam(1e-5))
+
 train_model.summary()
 
 
@@ -199,6 +224,26 @@ def evaluate(data=valid_generator):
     return 2 * (pre * rec) / (pre + rec)
 
 
+def write_to_file(pred, file_path='test_submission.tsv'):
+    """
+    将结果写入文件，方便提交
+    """
+    ret = []
+    for (d, p) in zip(test_data, pred):
+        one_ret = []
+        q_id = d['query_id']
+        reply = d['reply']
+        for i, r in enumerate(reply):
+            one_ret.append([q_id, r[0], p[i]])
+
+        ret.extend(one_ret)
+
+    # write to file
+    with open(file_path, 'w') as f:
+        for r in ret:
+            f.write('\t'.join([str(i) for i in r]) + '\n')
+
+
 class Evaluator(keras.callbacks.Callback):
     def __init__(self):
         self.best_f1 = 0.
@@ -220,5 +265,15 @@ if __name__ == '__main__':
         epochs=10,
         callbacks=[evaluator]
     )
+    # load best weights
+    train_model.load_weights('best_model.weights')
+    # pred
+    test_pred = []
+    for (t, s, l, m), _ in tqdm(test_generator):
+        p = predict([t, s, m])
+        test_pred.extend(p)
+
+    # write to file
+    write_to_file(test_pred)
 else:
     infer_model.load_weights('best_model.weights')
