ee018b9d57a2539bb06a1ccd36d386ea32203e6c
xv44586 <xv44586@gmail.com>
2020-12-11 11:27:14 +0800

    add  keras optimizers

--- toolkit4nlp/optimizers.py
+++ toolkit4nlp/optimizers.py
@@ -7,6 +7,7 @@ import re
 
 import numpy as np
 import tensorflow as tf
+from keras.optimizers import *
 
 from toolkit4nlp.backend import keras, K, is_tf_keras, piecewise_linear
 from toolkit4nlp.utils import *
